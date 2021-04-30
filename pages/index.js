b3d15d2226614d4b7a5fcd47864fd83e3edbf63b
Selwyn <talk@selwyn.cc>
2021-04-30 13:14:55 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function IndexPage() {
+  return (
+    <>
+      <Head>
+        <title>Flow Mapper</title>
+        <link rel="icon" href="/favicon.svg"></link>
+      </Head>
+    </>
+  );
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Flow Mapper
+> A tool aiming to enable the Fast-Moving Consumer Goods sector to transition to the Circular Economy
+
+Prototype by De Voorhoede in collaboration with Dyson School of Design Engineering.
