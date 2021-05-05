4bbc3d7eb3b225c78a9d9a9493ec5b3bc2de1260
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2021-05-05 10:41:30 +0200

    fix(phaseSlug): navigation & css fixes

--- pages/suggestion/[phaseSlug]/index.js
+++ pages/suggestion/[phaseSlug]/index.js
@@ -12,12 +12,12 @@ export default function SlugPage({ params, phase, allPhases }) {
         <link rel="icon" href="/favicon.svg"></link>
       </Head>
       <AppHeader />
-      <PhaseNavigation phases={allPhases} activeSlug={params.phaseSlug} />
-      <main>
-        <h2 className="p-4 text-lg font-bold">Suggestions</h2>
+      <main className="p-4">
+        <PhaseNavigation phases={allPhases} activeSlug={params.phaseSlug} />
+        <h2 className="my-4 text-lg font-bold">Suggestions</h2>
         {
           phase.suggestions.length
-          ? <ul className="px-4">
+          ? <ul>
             {
               phase.suggestions.map((suggestion) => {
                 if (suggestion.__typename === 'ProcessRecord') {
@@ -35,7 +35,7 @@ export default function SlugPage({ params, phase, allPhases }) {
                       style={{ background: suggestion.systemClass.color.hex }}
                       className="inline-block w-6 h-6 mr-2 rounded-full"
                     />
-                    <Link href={`/system/${suggestion.slug}`}>
+                    <Link href={`/suggestion/${params.phaseSlug}/${suggestion.slug}`}>
                       <a>{suggestion.name}</a>
                     </Link>
                   </li>
