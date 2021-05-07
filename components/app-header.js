b155ca89c1bdd5b421761128d7e939df7abb7831
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2021-05-07 16:44:44 +0200

    feat(app-header): hide the header in iframe's

--- components/app-header.js
+++ components/app-header.js
@@ -1,8 +1,18 @@
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
 export function AppHeader() {
+  const [isInIframe, setIsInIframe] = useState(0);
+
+  useEffect(() => {
+    setIsInIframe(window.location !== window.parent.location)
+  });
+
   return (
-    <header className="p-4 bg-gray-800 text-white">
+    <header
+      style={{ display: isInIframe ? 'none' : 'block' }}
+      className="p-4 bg-gray-800 text-white"
+    >
       <Link href="/">
         <a><h1 className="text-xl">Flow Mapper Assistant</h1></a>
       </Link>
